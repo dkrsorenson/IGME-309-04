9eda4ac808758ae5437bf17bb6f44cb5cd81ed5c
dkrsorenson <dkrsorenson@gmail.com>
2019-08-29 23:20:44 -0400

    Exercise 1, Visual Studio 2017 Configuration

---
+++ E1_VisualStudioConfig/E1_VisualStudioConfig/Main.cpp
@@ -0,0 +1,14 @@
+// E1_VisualStudioConfig: This file contains the 'main' function. Program execution begins and ends there.
+// Dakota Sorenson, DSA2 Section 4
+
+#include "Main.h"
+using namespace std;
+
+int main() {
+	cout << "Hello World\n";
+	
+	// Ending the program
+	cout << "Press enter to finish";
+	getchar();
+	return 0;
+}
