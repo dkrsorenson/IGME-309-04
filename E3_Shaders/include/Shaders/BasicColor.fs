20457eaa8adf8afb2f7049843414557d7b1a7484
dkrsorenson <dkrsorenson@gmail.com>
2019-09-14 11:43:15 -0400

    Exercise 3, Shaders

---
+++ E3_Shaders/include/Shaders/BasicColor.fs
@@ -0,0 +1,31 @@
+#version 330
+
+in vec3 Color;
+
+uniform vec3 SolidColor = vec3(-1,-1,-1);
+uniform bool ComplementaryColor = false;
+
+out vec4 Fragment;
+
+void main()
+{
+	Fragment = vec4(Color,1);
+	
+	if(ComplementaryColor) {
+		vec3 newColor = Color - vec3(1,1,1);
+		newColor *= -1;
+		Fragment = vec4(newColor,1);
+	}
+	
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0){
+		Fragment = vec4(SolidColor, 1);
+		if(ComplementaryColor) {
+			vec3 newColor = SolidColor - vec3(1,1,1);
+			newColor *= -1;
+			Fragment = vec4(newColor,1);
+		}
+	}
+	
+
+	return;
+}
