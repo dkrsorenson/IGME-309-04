5915c5240e249f496d3fd86d1c7f697a6a5d9041
dkrsorenson <dkrsorenson@gmail.com>
2019-10-01 21:43:19 -0400

    Exercise 5.5, lerping

--- Simplex_2188-master/E05.5 - LERP/AppClass.cpp
+++ Simplex_2188-master/E05.5 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Dakota Sorenson - drs4149@rit.edu";
 
 	//Set the position and target of the camera
 	m_pCameraMngr->SetPositionTargetAndUpward(vector3(5.0f,3.0f,15.0f), ZERO_V3, AXIS_Y);
@@ -46,30 +46,37 @@ void Application::Display(void)
 	// Draw the model
 	m_pModel->PlaySequence();
 
-	//Get a timer
-	static float fTimer = 0;	//store the new timer
+	// Get a timer
+	static float timer = 0;	//store the new timer
 	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
-	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+	timer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
 
-	//calculate the current position
-	vector3 v3CurrentPos;
+	// Calculate the current position
+	static int index = 0;
+	vector3 previousPos = m_stopsList[index];
+	vector3 nextPos = m_stopsList[(index + 1) % m_stopsList.size()];
+	vector3 currentPos;
 	
+	// Map the value to be between 0.0 and 1.0
+	float percentage = MapValue(timer, 0.0f, 2.0f, 0.0f, 1.0f);
 
-
-
-
-	//your code goes here
-	v3CurrentPos = vector3(0.0f, 0.0f, 0.0f);
-	//-------------------
+	// Lerp
+	currentPos = glm::lerp(previousPos, nextPos, percentage);
 	
-
-
-	
-	matrix4 m4Model = glm::translate(v3CurrentPos);
+	// Move character to calculated pos
+	matrix4 m4Model = glm::translate(currentPos);
 	m_pModel->SetModelMatrix(m4Model);
 
+	// If we've reached the next pos, update the index so we can start moving to the next point
+	if (percentage >= 1.0f)
+	{
+		index++; // increase index to next point
+		timer = m_pSystem->GetDeltaTime(uClock); // restart the clock
+		index %= m_stopsList.size(); // make sure we are within boundries
+	}
+
 	m_pMeshMngr->Print("\nTimer: ");//Add a line on top
-	m_pMeshMngr->PrintLine(std::to_string(fTimer), C_YELLOW);
+	m_pMeshMngr->PrintLine(std::to_string(timer), C_YELLOW);
 
 	// Draw stops
 	for (uint i = 0; i < m_stopsList.size(); ++i)
